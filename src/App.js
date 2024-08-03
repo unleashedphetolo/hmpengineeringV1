2fe3f91c91073d2f088856c4e26052f036fff985
Phetolo Mpe <83431769+skyvaccum@users.noreply.github.com>
2024-08-03 04:34:28 +0200

    Marketing features

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
           <Navbar.Collapse id='navbarScroll'>
             <Nav
               className='me-auto my-2 my-lg-0'
-              style={{ maxHeight: '100px' }}
+              style={{ maxHeight: 'auto' }}
               navbarScroll
             >
               <Link className='nav-link' to='/'>
